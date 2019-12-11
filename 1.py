09dcba03d7076859f873a07effddc968673a567e
Andrew <dreammakmuss@yandex.ru>
2019-12-11 20:51:41 +0300

    Это просто тестовый комментарий. Проверка работы git.

---
+++ 1.py
@@ -0,0 +1,11 @@
+month_days = int(input('Сколько дней в месяце?'))
+summa = 0
+
+for count in range (1, month_days):
+    summa += month_days
+
+print ('Сумма всех дней в месяце равна ', summa)
+
+
+
+
