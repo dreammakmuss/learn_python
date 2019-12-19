579b5b4a9b3d5f13df79ca804241a5df35c3023e
dreammakmuss <dreammakmuss@yandex.ru>
2019-12-19 22:20:24 +0300

    Just learn

--- functions/learn.py
+++ functions/learn.py
@@ -2,9 +2,10 @@ def main():
 	age1 = int(input('Enter your age:'))
 	age2 = int(input('Enter age of your friend:'))
 	total = sum(age1, age2)
-
+	print ('Всего вам столько лет:', total)
 
 def sum (num1, num2):
 	together = num1 + num2
 	return (together)
 
+main ()
